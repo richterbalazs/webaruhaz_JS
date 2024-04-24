9ad2d1d66b2d0eb50edfa7acbf919fc090f8075e
richterbalazs <richter.balazs.krisztian@gmail.com>
2024-04-24 15:05:20 +0200

    index.html felepitese kesz

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-import { POLOKLISTA } from "./adat";
-import { kartyaRendezes, megjelenit } from "./kartyaMegjelenit";
+import { POLOKLISTA } from "./adat.js";
+import { kartyaRendezes, megjelenit } from "./kartyaMegjelenit.js";
 
 function init(lista){
     megjelenit(kartyaRendezes(lista));
 }
 
-init(POLOKLISTA)
+init(POLOKLISTA);
