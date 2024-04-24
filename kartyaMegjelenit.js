ccd21201c5ede5207653d8f774a99e56767c532b
richterbalazs <richter.balazs.krisztian@gmail.com>
2024-04-24 21:18:46 +0200

    javitasok

--- kartyaMegjelenit.js
+++ kartyaMegjelenit.js
@@ -5,7 +5,7 @@ export function kartyaRendezes(lista){
         txt += `<div class="card col-md-4">`
         txt += `<div class="card-header">${lista[i].nev}</div>`
         txt += `<div class="card-body"><img src="${lista[i].kep}"class="kepek" ></div>`
-        txt += `<div class="card-footer">${lista[i].ar} Ft</div> <br>`
+        txt += `<div class="card-footer">Ár: ${lista[i].ar} Ft</div> <br>`
         txt += `<div><button id="${i}">Kosárba</button></div>`
         txt += `</div>`
     }
